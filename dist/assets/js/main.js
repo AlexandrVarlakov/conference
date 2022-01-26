bf7963453d32fa3f5e6488a09fa56ef232eb1759
AlexandrVarlakov <66496459+AlexandrVarlakov@users.noreply.github.com>
2022-01-26 06:27:06 +0300

    create pages with form + create page key speakers

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -68,3 +68,93 @@ if ( hamburger && mobmenu){
         
     }
 }
+
+let addFile = document.querySelectorAll('.add-file');
+let inputFile = document.querySelectorAll('.file-input');
+
+
+if ( inputFile ){
+    inputFile.forEach( (input) => {
+        input.onchange = function(){
+            let fileContainer = input.parentElement;
+            let btn = fileContainer.querySelector('.add-file');
+            btn.dataset.file = input.files[0].name;
+
+
+            let span = document.createElement('span');
+            span.innerText = input.files[0].name;
+            fileContainer.prepend(span);
+
+        }
+    })
+}
+
+if ( addFile ) {
+
+    addFile.forEach( (btn) => {
+        btn.onclick = function(e){
+
+            if ( btn.hasAttribute('data-file') === false){
+                let fileContainer = btn.parentElement;
+            
+                let fileInput = fileContainer.querySelector('.file-input');
+
+                fileInput.click();
+            } else {
+                btn.removeAttribute('data-file');
+                let fileContainer = btn.parentElement;
+            
+                let fileInput = fileContainer.querySelector('.file-input');
+                fileInput.value = null;
+
+                let span = fileContainer.querySelector('span');
+                if (span){
+                    span.remove();
+                }
+            }
+
+
+            
+
+        }
+    })
+
+}
+
+let textApprove = document.querySelectorAll('.easyCheckbox__text');
+
+if ( textApprove ){
+    textApprove.forEach( (text) => {
+        text.onclick = function(e){
+
+
+            if ( e.target.tagName == 'a' || e.target.tagName == 'A'){
+                
+            } else {
+                e.preventDefault();
+            }
+        }
+    })
+}
+
+let inpApprove = document.querySelector('#approve');
+let formSendBtn = document.querySelector('.sendform-btn');
+
+
+if ( inpApprove ){
+    if ( inpApprove.checked ){
+        formSendBtn.removeAttribute('disabled');    
+    } else{
+        formSendBtn.setAttribute('disabled', 'disabled');
+    }
+
+
+    inpApprove.onchange = function(){
+        if ( inpApprove.checked ){
+            formSendBtn.removeAttribute('disabled');    
+        } else{
+            formSendBtn.setAttribute('disabled', 'disabled');
+        }
+    }
+}
+
